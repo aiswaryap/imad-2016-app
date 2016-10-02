4fbdb19d222fb221a3301af655c9af3183be34f4
aiswaryap <aiswaryasugathan001@gmail.com>
2016-10-02 11:53:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+console.log('Loaded!');
+
+//change the text of the main script
+var element = document.getElementById('main-text');
+element.innerHTML = 'new value';
+
+//move image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft=marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function() {
+     var interval = setInterval(moveRight, 50);
+};
