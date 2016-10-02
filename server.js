eac23b558e5fa19b0b67a6accb97e3a0390a9641
aiswaryap <aiswaryasugathan001@gmail.com>
2016-10-02 11:16:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/main.js', function (reg, res){
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+ app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
