4d33c641994db2e1a5464dd3ca943bbbefbf8a8d
aiswaryap <aiswaryasugathan001@gmail.com>
2016-10-03 19:26:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.get('/counter', function(req, res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req, res) {
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
+
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
